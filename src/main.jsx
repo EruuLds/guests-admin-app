6d1ab02f375141437f7e8aeefc1b917fd9248515
Hugo Erhubey Quintana Ledesma <eruledesma@MacBook-Air-de-Hugo.local>
2025-12-27 00:01:46 -0600

    Styles importing bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css';
+import './App.css';
 import App from './App.jsx'
 import { DataContextProvider } from './contexts/DataContext.jsx'
 import { ModalContextProvider } from './contexts/ModalContext.jsx'
