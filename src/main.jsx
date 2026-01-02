6ac8f2a7348b3c25b0bd5834fe74900fc7c21154
Hugo Erhubey Quintana Ledesma <eruledesma@MacBook-Air-de-Hugo.local>
2026-01-01 23:03:49 -0600

    A bug related to CSS files has been fixed.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css';
-import './App.css';
 import App from './App.jsx'
 import { DataContextProvider } from './contexts/DataContext.jsx'
 import { ModalContextProvider } from './contexts/ModalContext.jsx'
