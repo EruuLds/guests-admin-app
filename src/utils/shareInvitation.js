cb2b7692754b88a1a6d50c522b5b0559a5ccf9c0
Hugo Erhubey Quintana Ledesma <eruledesma@MacBook-Air-de-Hugo.local>
2026-01-21 04:09:19 -0600

    Change in the custom mesagge when an invitation is shared

--- src/utils/shareInvitation.js
+++ src/utils/shareInvitation.js
@@ -5,7 +5,7 @@ import { shareInvitationViaWhatsApp } from "./shareInvitationViaWhatsapp";
 export function shareInvitation(guestId, guestName) {
     const { customInvitationURL } = useContext(DataContext);
     const guestURL = customInvitationURL + '?id=' + guestId;
-    const message = `¡Hola, ${guestName}!\n\nQueremos compartir contigo un momento muy especial para nosotros. Es por eso que compartimos el enlace a nuestra invitación digital con todos los detalles y la confirmación de asistencia:\n\n${guestURL}\n\n¡Nos haría muy felices que nos acompañes!\n\nCon cariño,\n*Ale & Luis.*`
+    const message = `¡Hola, ${guestName}!\n\n¡Nos casamos! Y queremos celebrar contigo este momento tan especial en nuestras vidas. Te compartimos aquí el enlace a nuestra invitación digital con todos los detalles y la confirmación de asistencia:\n\n${guestURL}\n\n¡Nos haría muy felices que nos acompañes!\n\nCon cariño,\n*Ale & Luis.*`;
 
     const share = async () => {
     if (navigator.share) {
